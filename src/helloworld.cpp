26c3dc810c83bafc2c9c6b803096df2aba4d3041
weimuhua <weimuhua0812@126.com>
2023-11-20 13:42:42 +0800

    feat: 定义常亮

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -2,10 +2,19 @@
 #include <vector>
 #include <string>
 
+#define day 7
 using namespace std;
 
 int main()
 {
+    cout << "const day = " << day << endl;
+
+    const int week = 52;
+    cout << "const week = " << week << endl;
+
+    int a = 10;
+    cout << "a = " << a << endl;
+
     std::cout << "hello, world\n";
     return 0;
 }
