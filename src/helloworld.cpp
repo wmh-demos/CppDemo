fcdab9b226012ab98797657440c7ff95a0884204
weimuhua <weimuhua0812@126.com>
2023-11-21 12:57:33 +0800

    feat: add sizeof

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -8,6 +8,9 @@ using namespace std;
 int main()
 {
     cout << "const day = " << day << endl;
+    cout << "sizeof(day) = " << sizeof(day) << endl;
+    cout << "sizeof(int) = " << sizeof(int) << endl;
+    cout << "sizeof(long) = " << sizeof(long) << endl;
 
     const int week = 52;
     cout << "const week = " << week << endl;
